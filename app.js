169990162d781cc9d441486240469a91ea5af56c
sait <sait.bozteke63@gmail.com>
2022-02-15 23:25:27 +0100

    file added

--- app.js
+++ app.js
@@ -3,30 +3,27 @@ const input = document.querySelector("#add");
 const btn = document.querySelector("#btn");
 const list = document.querySelector("#list");
 
-//eventlisteners
-btn.addEventListener("click", btnClick());
-list.addEventListener("click", listClick());
+//event listeners
+btn.addEventListener("click", btnClick);
+list.addEventListener("click", listClick);
 
 //functions
 
 function btnClick() {
   var txt = input.value;
   if (txt == "") {
-    alert("You must write something!");
+    alert("you must write something!");
   } else {
     li = document.createElement("li");
     li.classList.add("element-group__list__item");
     li.innerHTML = txt;
-    list.insertBefore("li", list.childNodes[0]);
+    list.insertBefore(li, list.childNodes[0]);
     input.value = "";
   }
 }
 
-function listClick(e){
-if(e.target.tagName=="LI"){
-e.target.classList.toggle("checked")
-
-}
-
-
+function listClick(e) {
+  if (e.target.tagName == "LI") {
+    e.target.classList.toggle("checked");
+  }
 }
